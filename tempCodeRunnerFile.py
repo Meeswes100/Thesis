c860190ace941ecc96b99727ed2fc150bceaea1e
meeswes100 <mees@famwesseling.nl>
2026-03-09 15:55:04 +0100

    remove vscode and temp files

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-high_corr = (
-    corr.abs()
-        .where(np.triu(np.ones(corr.shape), k=1).astype(bool))
-        .stack()
-        .sort_values(ascending=False)
-)
-
-print("correlation")
-print(high_corr.head(20))
